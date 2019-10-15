efd876bf4bcebf3964f1ea9e5fb27e7deb9079c3
胡艺兴 <562157456@qq.com>
2019-10-15 17:32:42 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    HOST: 'localhost',
+    port: 3000,
+    proxy: undefined
+}
---
+++ src/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React, { SFC } from 'react'
+
+interface ILayoutProps {}
+
+const Layout:SFC<ILayoutProps> = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Layout

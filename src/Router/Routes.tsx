3bb354845df6e6deddc04a9aa77b714aa129e877
hyx <562157456@qq.com>
2019-11-04 14:22:03 +0800

    feat:新增antd4.0依赖

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import Layout from 'Layout'
+import React from "react"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
+import Layout from "Layout"
 
 const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" component={Layout}  />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Layout} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default Routes
